17c6b5f5bc306d3043d58ecad706d8b83079658b
joseph M. Honea <jhonea@apexturbine.com>
2018-07-20 00:51:46 -0500

    Completed neural nets

--- 03 - Data Analysis/3-2 Neural Nets/NeuralNets.py
+++ 03 - Data Analysis/3-2 Neural Nets/NeuralNets.py
@@ -11,13 +11,17 @@ class NeuralNet:
 
     def sigmoid_prime(self,x):
         return x*(1-x)
-        
-    def Run(self, data, answer):
-        #Feed Forward
+
+    def feed_forward(self,data):
         layers = []
         layers.append(data)
         for l in range(self.numLayers-1):
             layers.append(self.sigmoid(np.dot(layers[l],self.weights[l])))
+        return layers
+        
+    def train(self, data, answer):
+        #Feed Forward
+        layers = self.feed_forward(data)
             
         out_error = answer - layers[-1]
         out_delta = out_error * self.sigmoid_prime(layers[-1])
